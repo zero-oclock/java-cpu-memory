d7c8f9d8bd38af4d117545bca38f60da76970614
linjunbo <linjunbo@babyfs.cn>
2020-06-04 16:33:08 +0800

    优化cpu负载分析脚本

--- show_maximum_cpu.sh
+++ show_maximum_cpu.sh
@@ -27,11 +27,26 @@ process_top() {
 	sed -i "1,${start}d" top.tmp
 }
 
+process_stack() {
+	nid=$1
+	index=0
+	start=0
+	while read -r line
+	do
+		index=`expr $index + 1`
+		if [[ $start -eq 0 ]] && [[ $line =~ "prio=" ]] && [[ $line =~ "tid=" ]] && [[ $line =~ "nid=" ]] && [[ ! $line =~ "nid=$nid" ]]; then
+                        start=$index
+                fi
+	done < cpu_result_tmp.log
+	sed -i "${start},${index}d" cpu_result_tmp.log
+}
+
 process_top
 sort -rn -k 9  top.tmp | grep java |  awk -F ' ' '{print $1 "\t" $9}' | head -5 > top.result
 rm -rf top.tmp
 
 #遍历5个进程，获取占用CPU超30%的进程中CPU占用最高的5个线程
+echo -e "\n \e[1;33;41m 分析CPU占用最高的5个进程，取其中占用率超过${p_threshold}%的进程: \e[0m"
 while read -r line
 do
 	pid=`echo $line | awk -F ' ' '{print $1}'`
@@ -52,6 +67,7 @@ do
 done < top.result
 
 #打印出占用CPU最高的5个线程中CPU占用超过20%的线程
+echo -e "\n \e[1;33;41m 分析CPU占用率超过${p_threshold}%的进程，其中CPU占用率超过${t_threshold}%的线程: \e[0m"
 touch top_Hp.result
 while read -r line
 do
@@ -66,5 +82,11 @@ do
 	echo "进程:${pid}    线程:${tid}  负载:${cpu_load}%  大于${t_threshold}%"
 	pid_hex=`printf "%x\n" $tid`
 	pid_hex="0x"${pid_hex}
-	jstack $pid | grep -A10 $pid_hex >> cpu_result.log
+	echo -e ">>>>>>>>>>>>>>>>高CPU线程:" >> cpu_result.log
+	jstack $pid | grep -A200 $pid_hex >> cpu_result_tmp.log
+	process_stack $pid_hex
+	cat cpu_result_tmp.log >> cpu_result.log
+	rm -rf cpu_result_tmp.log
 done < top_Hp.result
+
+echo -e "\n\n请打开cpu_result.log文件查看CPU高负载堆栈信息\n\n"
